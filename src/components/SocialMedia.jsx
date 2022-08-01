6a4ea7e45f6adb7089008a6030f6a8bec251ef35
ST <santiago.b.toloza@gmail.com>
2022-07-31 23:09:48 -0300

    agregada las nuevas pestañas

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -5,20 +5,19 @@ const SocialMedia = () => {
   return (
     <div className="app__social">
       <div>
-        <a href="https://www.linkedin.com/in/santiagotoloza/">
+        <a href="https://www.linkedin.com/in/santiagotoloza/" target="_blank">
           <BsLinkedin></BsLinkedin>
         </a>
       </div>
       <div>
-        <a href="https://www.facebook.com/santiagotooloza/">
+        <a href="https://www.facebook.com/santiagotooloza/" target="_blank">
           <FaFacebookF />
         </a>
       </div>
       <div>
-        <a href="https://www.instagram.com/santiagotoloza_/">
+        <a href="https://www.instagram.com/santiagotoloza_/" target="_blank">
           <BsInstagram />
         </a>
-        <a></a>
       </div>
     </div>
   );
