3f1d0ecb643201a931b8bd40c61f85a172540441
st1923 <santiagotoloza@hotmail.com>
2022-08-13 12:57:41 -0300

    add cv

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -24,8 +24,8 @@ const Header = () => {
             </div>
           </div>
           <div className="download-cv">
-              <button className="button-cv ">Download CV</button>
-              <button className="button-cv ">View Online</button>
+              <button className="button-cv"> <a href="https://drive.google.com/uc?id=1ec-r2p22ryKtfKbU-ONNlSvfP_pdX94o&export=download" >Download CV</a> </button>
+              <button className="button-cv"><a href="https://drive.google.com/file/d/1ec-r2p22ryKtfKbU-ONNlSvfP_pdX94o/view?usp=sharing" target="_blank">View Online</a> </button>
           </div>
         </div>
       </motion.div>
